9f5c9a3d2d68df2a3fbdf038306154615090d8bb
wannakorn1101 <95087020+wannakorn1101@users.noreply.github.com>
2022-06-25 14:31:45 +0700

    week10-1

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,16 @@ use App\Http\Controllers\Covid19Controller;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;  
+use App\Http\Controllers\UserController;  
+use App\Http\Controllers\VehicleController;  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
 Route::get('/', function () {
     return view('welcome');
@@ -152,3 +153,9 @@ Route::resource('/covid19', Covid19Controller::class );
 //week7
 Route::resource('/staff', StaffController::class );
 Route::resource('post', PostController::class);
+
+//week10
+Route::resource('profile', ProfileController::class);
+Route::resource('user', UserController::class);
+Route::resource('vehicle', VehicleController::class);
+
